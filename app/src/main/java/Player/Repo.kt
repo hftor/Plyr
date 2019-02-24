d9943638c332542cf99f4b9b0cc7162ab0d1f114
Haftor <haftor.g@gmail.com>
2019-02-24 12:59:43 +0000

    Playes first song that we find.

--- app/src/main/java/Player/Repo.kt
+++ app/src/main/java/Player/Repo.kt
@@ -1,11 +1,11 @@
 package Player
 
-import android.content.ContentResolver
 import android.content.Context
 import android.media.MediaPlayer;
 import com.mtechviral.mplaylib.MusicFinder
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.*
-
 
 /**
  * Created by hafthorg on 22/02/2019.
@@ -13,11 +13,31 @@ import kotlinx.coroutines.*
 class Repo(private val context: Context) {
 
     private var mediaPlayer : MediaPlayer? = null
+    private var songs : MutableList<MusicFinder.Song>? = null
 
     fun getMessage() : String{
+        val ss = GlobalScope.async{
+            val songFinder = MusicFinder(context.contentResolver)
+            songFinder.prepare()
+            songFinder.allSongs
+            songs = songFinder.allSongs
+        }
 
+        var a = runBlocking {
+            ss.await()
+            var firstSong = songs?.get(0)
+            if(firstSong == null)
+            {
+                 "meee"
+            }
+            else{
+                mediaPlayer = MediaPlayer.create(context, firstSong.uri)
+                mediaPlayer?.start()
 
-        return "This is a repo message from context: " + context.applicationContext.toString()
+                "This is a repo message from async: " + songs?.get(0)?.artist
+            }
+        }
+
+        return a
     }
-
 }
--- app/src/main/java/com/hftor/plyr/MainActivity.kt
+++ app/src/main/java/com/hftor/plyr/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.hftor.plyr
 
 import Player.PlayerViewModel
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +18,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), Context.CONTEXT_INCLUDE_CODE)
+        }
 
         val a = p.loadData()
 
