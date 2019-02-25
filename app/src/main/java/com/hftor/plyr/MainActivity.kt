4d521c1b0a9f3746f985a24194b501fa34fd127b
Haftor <haftor.g@gmail.com>
2019-02-25 16:43:26 +0000

    Moves viewModel to fragment.

--- app/src/main/java/com/hftor/plyr/MainActivity.kt
+++ app/src/main/java/com/hftor/plyr/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.hftor.plyr
 
-import Player.PlayerViewModel
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
@@ -8,19 +7,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.NavHostFragment
-import kotlinx.android.synthetic.main.activity_main.*
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
-
-    private val p : PlayerViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         val host = NavHostFragment.create(R.navigation.nav_graph)
         supportFragmentManager.beginTransaction().replace(R.id.mainContainer, host).setPrimaryNavigationFragment(host).commit()
@@ -29,18 +22,6 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), Context.CONTEXT_INCLUDE_CODE)
         }
 
-        p.getCurrentSong().observe(this, Observer {
-            song ->
-            //testText.text = song.title
-        })
-
-//        button.setOnClickListener {
-//            playNext()
-//        }
-    }
-
-    private fun playNext(){
-        p.playNext()
     }
 
 }
--- app/src/main/java/com/hftor/plyr/PlayerFragment.kt
+++ app/src/main/java/com/hftor/plyr/PlayerFragment.kt
@@ -1,10 +1,14 @@
 package com.hftor.plyr
 
+import Player.PlayerViewModel
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.fragment_player.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 /**
@@ -18,9 +22,21 @@ import android.view.ViewGroup
  */
 class PlayerFragment : Fragment() {
 
+    private val p : PlayerViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        p.getCurrentSong().observe(this, Observer {
+            song ->
+            title.text = song.title
+        })
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_player, container, false)
+
     }
 }
