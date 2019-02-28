be1ab06aef29ec6a0c2c948ddf9a68723c10c138
Haftor <haftor.g@gmail.com>
2019-02-28 12:14:53 +0000

    Refactors getting songs.

--- app/src/main/java/Player/Repo.kt
+++ app/src/main/java/Player/Repo.kt
@@ -5,9 +5,6 @@ import android.media.MediaPlayer
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.mtechviral.mplaylib.MusicFinder
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.*
 
 /**
  * Created by hafthorg on 22/02/2019.
@@ -24,25 +21,11 @@ class Repo(private val context: Context) {
     private var mediaPlayer : MediaPlayer? = null
 
     init {
-        runBlocking {
-            initializeMediaPlayer()
-        }
-    }
+        var songFinder = MusicFinder(context.contentResolver)
+        songFinder.prepare()
 
-    suspend fun initializeMediaPlayer(){
-        val deferred = GlobalScope.async{
-            val songFinder = MusicFinder(context.contentResolver)
-            songFinder.prepare()
-            songFinder.allSongs
-            _songs = songFinder.allSongs
-        }
-
-
-        runBlocking {
-            deferred.await()
-
-            songs.value = _songs
-        }
+        _songs = songFinder.allSongs
+        songs.value = _songs
     }
 
     private fun setCurrentSong(songId: Long){
