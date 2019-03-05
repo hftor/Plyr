53abbf30b85325cd80a4d1ea489dd8e06c882120
Haftor <haftor.g@gmail.com>
2019-03-05 10:58:53 +0000

    Refactors fetcing from roomDB.

--- app/src/main/java/com/hftor/plyr/dataBase/AppDataBase.kt
+++ app/src/main/java/com/hftor/plyr/dataBase/AppDataBase.kt
@@ -13,20 +13,32 @@ abstract class AppDataBase : RoomDatabase() {
     abstract fun songInfoDao() : SongInfoDao
 
     companion object {
-        var INSTANCE: AppDataBase? = null
 
-        fun getAppDataBase(context: Context): AppDataBase? {
-            if (INSTANCE == null){
-                synchronized(AppDataBase::class){
-                    INSTANCE = Room.databaseBuilder(context.applicationContext, AppDataBase::class.java, "myDB")
+        var TEST_MODE = false
+
+        var db: AppDataBase? = null
+        var dbInstance: SongInfoDao? = null
+
+        fun getInstance(context: Context): SongInfoDao? {
+            if (db == null){
+                if(TEST_MODE){
+                    db = Room.inMemoryDatabaseBuilder(context.applicationContext, AppDataBase::class.java)
+                            .allowMainThreadQueries()
                             .build()
+                    dbInstance = db?.songInfoDao()
+                }
+                else{
+                    db = Room.databaseBuilder(context.applicationContext, AppDataBase::class.java, "myDB")
+                            .build()
+                    dbInstance = db?.songInfoDao()
                 }
             }
-            return INSTANCE
+
+            return dbInstance!!
         }
 
         fun destroyDataBase(){
-            INSTANCE = null
+            db = null
         }
     }
 }
--- app/src/main/java/com/hftor/plyr/repo/SongInfoRepository.kt
+++ app/src/main/java/com/hftor/plyr/repo/SongInfoRepository.kt
@@ -1,7 +1,6 @@
 package com.hftor.plyr.repo
 
 import android.content.Context
-import com.hftor.plyr.dao.SongInfoDao
 import com.hftor.plyr.dataBase.AppDataBase
 import com.hftor.plyr.entity.SongInfo
 import com.mtechviral.mplaylib.MusicFinder
@@ -11,15 +10,6 @@ import kotlinx.coroutines.launch
 
 class SongInfoRepository(private val context: Context) {
 
-
-    private var db: AppDataBase? = null
-    private var songInfoDao: SongInfoDao? = null
-
-    init {
-        db = AppDataBase.getAppDataBase(context)
-        songInfoDao = db?.songInfoDao()
-    }
-
     fun saveSongsState(songToSave : MusicFinder.Song?, songPosition : Int?){
         if(songToSave == null || songPosition == null || songPosition == 0){
             return
@@ -27,13 +17,13 @@ class SongInfoRepository(private val context: Context) {
 
         GlobalScope.launch {
             var s1 = SongInfo(songToSave.id, songPosition)
-            songInfoDao?.insertSongInfo(s1)
+            AppDataBase.getInstance(context)?.insertSongInfo(s1)
         }
     }
 
     suspend fun getSongPosition(songId: Long) : Int{
         var songPosDeferred = GlobalScope.async {
-            songInfoDao?.getSongInfo(songId)?.position ?: 0
+            AppDataBase.getInstance(context)?.getSongInfo(songId)?.position ?: 0
         }
 
         return songPosDeferred.await()
